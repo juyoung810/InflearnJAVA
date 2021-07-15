a6879aaab88153e4f5475ba34f53621376b8820a
juyoung <kjy2967@naver.com>
2021-07-15 15:21:45 +0900

    comment 추가

--- JavaTPCProject/src/Project02_A.java
+++ JavaTPCProject/src/Project02_A.java
@@ -16,8 +16,11 @@ public class Project02_A {
             e.printStackTrace();
         }
         // 뉴스의 전체 목록 가져올 수 있는 element
+        // div 태그의 이름이 home_hit_grid 중 index가 0 인것
         Elements element=doc.select("div.home_hit_grid").eq(0);
+        // 해당 div 태그 밑의 h3 태그 의 text에서 0 ~ 8 글자
         String title = element.select("h3").text().substring(0,8); // 0~8 8개의 글자 타이틀로 받는다.
+        // 해당 태그중 ul 태그에서 index 0인 li 태그
         title += "-" + element.select("ul.heading_tab_lst").select("li").eq(0).text();
         System.out.println("==============================================");
         System.out.println(title);
--- JavaTPCProject/src/Project02_B.java
+++ JavaTPCProject/src/Project02_B.java
@@ -13,15 +13,22 @@ public class Project02_B {
         try{
             System.out.print("[입력력 YYYYMMDD] : ");
             String day = br.readLine();
+            // 날짜 입력 어떻게 넘기는지는 html 코드를 분석할 것
             url = url+"/"+day;
             System.out.println("==================================");
+            // 날짜를 입력 받아 보낸다.
             Document doc = Jsoup.connect(url).post();
             Element pan_text = doc.select(".depth3_tab_wrap").first();
+            // eq : 해당 태그 중 index를 선택한다.
             System.out.println(pan_text.select("li").eq(1).text());
 
             Element pan_date = doc.select(".date-sort2 strong").first();
             System.out.println(pan_date.text());
 
+
+            // . :class name, # : id 로 구별하여 태그 들고 오기
+            //  >  해당 태그의 자식 태그
+            // .first : class 이름은 중복이 가능하므로, 해당 클래스의 이름에 해당하는 것 중 첫번째를 말한다.
             Elements liList = doc.select(".list.list_type2 > li");
             for(Element li : liList)
             {
